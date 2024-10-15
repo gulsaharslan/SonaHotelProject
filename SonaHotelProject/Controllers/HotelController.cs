943c4859f893ad11cce5cbac7ebcf4d1746323dd
Gulsah <gulsharslan@gmail.com>
2024-10-15 15:05:14 +0300

    ..

--- SonaHotelProject/Controllers/HotelController.cs
+++ SonaHotelProject/Controllers/HotelController.cs
@@ -21,7 +21,7 @@ namespace SonaHotelProject.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?dest_id={cityId}&order_by=popularity&checkout_date={formattedCheckOutDate}&children_number=2&filter_by_currency=AED&locale=en-gb&dest_type=city&checkin_date={formattedCheckInDate}&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&include_adjacency=true&page_number=0&adults_number=2&room_number=1&units=metric"),
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?dest_id={cityId}&order_by=popularity&checkout_date={formattedCheckOutDate}&children_number=2&filter_by_currency=EUR&locale=en-gb&dest_type=city&checkin_date={formattedCheckInDate}&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&include_adjacency=true&page_number=0&adults_number=2&room_number=1&units=metric"),
                 Headers =
                         {
                             { "x-rapidapi-key", "fec9d14082msh2bae643a0505c69p1c2a50jsn4b0bb9d1f16d" },
